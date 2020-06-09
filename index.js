960e94e2d72ae234847203cfb9597224c7c12ed3
Ilshat Ishdavletov <lonlyd@gmail.com>
2020-06-09 14:59:38 +0500

    Did homework-2.

--- index.js
+++ index.js
@@ -1,66 +1,21 @@
-const fs = require('fs');
-const path = require('path');
+const http = require('http');
+const port = 3000;
+const delay = 1000;
+const limit = 5000;
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    let repeat = setInterval(() =>
+      console.log(new Date()), delay);
 
-const filesDir = './files';
-const finalDir = './final';
-const readDir = (filesDir) => {
-    const files = fs.readdirSync(filesDir);
-    files.forEach(element => {
-        let localBase = path.join(filesDir, element);
-        fs.stat(localBase, (err, stats) => {
-            if (err) {
-                console.log(err);
-                return;
-            } else {
-                if (stats.isDirectory()) {
-                    readDir(localBase);
-                } else {
-                    let filename = path.basename(element);
-                    let firstLetter = filename[0];
-                    let destination = path.join(finalDir, firstLetter, element);
-                    createDir(firstLetter, localBase, destination);
-                }
-            }
-        });
-    });
-}
+    setTimeout(() => {
+      clearInterval(repeat);
+      console.log('stop: ' + new Date());
+    }, limit);
+    
+    res.end("Homework #2");
+  };
+})
 
-// Make Final directory
-if (!fs.existsSync('final')) {
-    fs.mkdir('final', (err) => {
-        if (err) {
-            console.log(err)
-        }
-    });
-} else {
-    console.info('The "final" directory is exists');
-}
-
-const createDir = (firstLetter, localBase, destination) => {
-    let newDir = path.join(finalDir, firstLetter);
-    if (!fs.existsSync(newDir)) {
-        fs.mkdir(newDir, (err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('Create dir ' + firstLetter + ' done');
-                moveFiles(localBase, destination);
-            }
-        });
-    } else {
-        moveFiles(localBase, destination);
-        console.info('The directory is exists');
-    }
-}
-
-const moveFiles = (file, destination) => {
-    fs.rename(file, destination, (err) => {
-        if (err) {
-            return console.log(err)
-        } else {
-            console.info('Move files done!')
-        }
-    });
-}
-
-readDir(filesDir);
+server.listen(port, () => {
+  console.log("Server start on " + port + ' port');
+})
