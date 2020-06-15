f503c8269dccd7d93794e0a5d01bb10df95c61db
Ilshat Ishdavletov <lonlyd@gmail.com>
2020-06-15 21:55:37 +0500

    Add decrement and setTimeout

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ let userConnect = 0;
 const server = http.createServer((req, res) => {
   if ((req.method === 'GET') && (req.url === '/')) {
     userConnect++;
-    if (userConnect === 1) {
+    setTimeout(() => {
+      if (userConnect === 1) {
       let repeat = setInterval(() =>
         console.log(new Date()), delay);
 
       setTimeout(() => {
         clearInterval(repeat);
         console.log('stop: ' + new Date());
+        userConnect--;
       }, limit);
 
+    }},limit);
       res.end('Homework #2');
-    } else {
-      return;
-    }
   }
 })
 
